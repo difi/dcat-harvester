25e23b02abd3d001ede34781350785e44917f6fd
Øystein Høyum <oho@difi.no>
2015-12-17 10:11:33 +0100

    #3 Config for deploy on appserver

--- dcat-harvester-app/src/main/java/no/difi/dcat/harvester/Application.java
+++ dcat-harvester-app/src/main/java/no/difi/dcat/harvester/Application.java
@@ -2,14 +2,21 @@ package no.difi.dcat.harvester;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	
     public static void main(String[] args) {
     	
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
     }
 }
