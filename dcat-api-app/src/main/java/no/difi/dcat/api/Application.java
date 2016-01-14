4047ae7f76cc524e655ca7fce275bfd24703818a
Jan Bjørndalen <jan.bjorndalen@gmail.com>
2016-01-14 09:12:40 +0100

    #21 xml converter

--- dcat-api-app/src/main/java/no/difi/dcat/api/Application.java
+++ dcat-api-app/src/main/java/no/difi/dcat/api/Application.java
@@ -8,31 +8,38 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.oxm.castor.CastorMarshaller;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @SpringBootApplication
 public class Application extends SpringBootServletInitializer {
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
-    
-    @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-    	super.onStartup(servletContext);
-    	
-        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();  
-        ctx.register(AtomConfiguration.class);  
-        ctx.setServletContext(servletContext);    
-        Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));  
-        dynamic.addMapping("/");  
-        dynamic.setLoadOnStartup(1);  
-    }
-    
-    public static void main(String[] args) {
-    	
-        SpringApplication.run(Application.class, args);
-    }
+	@Bean
+	public CastorMarshaller castorMarshaller() {
+		return new CastorMarshaller();
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
+
+		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+		ctx.register(AtomConfiguration.class);
+		ctx.setServletContext(servletContext);
+		Dynamic dynamic = servletContext.addServlet("dispatcher", new DispatcherServlet(ctx));
+		dynamic.addMapping("/");
+		dynamic.setLoadOnStartup(1);
+	}
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+	}
 }
