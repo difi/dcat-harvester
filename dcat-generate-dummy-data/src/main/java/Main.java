be5b466a8350c0b0bfc7ab6b349e22463d2bb75e
Håvard Ottestad <hmottestad@gmail.com>
2016-01-22 13:33:29 +0100

    creating dcat class

--- dcat-generate-dummy-data/src/main/java/Main.java
+++ dcat-generate-dummy-data/src/main/java/Main.java
@@ -2,6 +2,8 @@ import no.difi.dcat.harvester.validation.DcatValidation;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
+import org.apache.jena.vocabulary.RDF;
 
 import java.util.UUID;
 
@@ -18,13 +20,20 @@ public class Main {
 
 
 		Resource resource = defaultModel.createResource(EXAMPLE + UUID.randomUUID().toString());
-		
+
+		resource.addProperty(RDF.type, DCAT.Catalog);
 
 
-		DcatValidation.validate(defaultModel, error -> System.out.println(error));
+		DcatValidation.validate(defaultModel, error -> {if(error.isError())System.out.println(error);});
 
 
 
+
+	}
+
+	static class DCAT{
+		static final String URI = "http://www.w3.org/ns/dcat#";
+		static final Resource Catalog = ResourceFactory.createResource(URI+"Catalog");
 
 	}
 
