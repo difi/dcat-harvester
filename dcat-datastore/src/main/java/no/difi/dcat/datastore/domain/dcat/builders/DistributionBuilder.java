89759aebc51299e3a915bda88049c6f24c2ff5cd
Jan Bjørndalen <jan.bjorndalen@gmail.com>
2016-02-05 09:01:14 +0100

    #55 Fixing so rss items don't get the correct dataset distributions format

--- dcat-datastore/src/main/java/no/difi/dcat/datastore/domain/dcat/builders/DistributionBuilder.java
+++ dcat-datastore/src/main/java/no/difi/dcat/datastore/domain/dcat/builders/DistributionBuilder.java
@@ -23,13 +23,13 @@ public class DistributionBuilder extends AbstractBuilder {
 	public List<Distribution> build() {
 		List<Distribution> distributions = new ArrayList<>();
 		
-		ResIterator catalogIterator = model.listResourcesWithProperty(RDF.type, DCAT.catalog);
+		ResIterator catalogIterator = model.listResourcesWithProperty(RDF.type, DCAT.Catalog);
 		while (catalogIterator.hasNext()) {
 			Resource catalog = catalogIterator.next();
-			ResIterator datasetIterator = catalog.getModel().listResourcesWithProperty(RDF.type, DCAT.dataset);
+			ResIterator datasetIterator = catalog.getModel().listResourcesWithProperty(RDF.type, DCAT.Dataset);
 			while (datasetIterator.hasNext()) {
 				Resource dataset = datasetIterator.next();
-				ResIterator distributionIterator = dataset.getModel().listResourcesWithProperty(RDF.type, DCAT.distribution);
+				ResIterator distributionIterator = dataset.getModel().listResourcesWithProperty(RDF.type, DCAT.Distribution);
 				while (distributionIterator.hasNext()) {
 					Resource distribution = distributionIterator.next();
 					distributions.add(create(distribution, dataset, catalog));
